a8cc9edeb0cfe0b0da0059dbc50a8f211ae6fcb2
unknown <itodistiano22@gmail.com>
2024-07-28 16:03:33 +0700

    edit img jumbotron 2

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -20,7 +20,7 @@ const inriasans = Inria_Sans({
   weight: ['300', '400', '700'],
 });
 
-const images = ['/images/display.png', '/images/gallery/foto1.jpg', '/images/gallery/foto3.jpg', '/images/gallery/foto4.jpg', '/images/gallery/foto5.jpg'];
+const images = ['/images/display.png', '/images/gallery/foto1.jpg', '/images/gallery/foto4.jpg'];
 
 export default function Home() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
